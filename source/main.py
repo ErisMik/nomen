76cfe2f4877d5432249794a98aa67f8858b62988
Eric Mikulin <itsEris>
2016-01-12 20:20:55 +0000

    Intentionally broke code to test CI

--- source/main.py
+++ source/main.py
@@ -9,4 +9,5 @@ from library import graphics
 # Main method
 if __name__ == "__main__":
 	# TODO: Check for and install dependencies
+	...
     APPLICATION = graphics.AppManager()  # Start the main application
